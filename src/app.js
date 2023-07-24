1e62894390fbcd8ef172b7eb2522e3b5fa8b7138
shubham2o <shubhamsharma.ha@gmail.com>
2023-07-25 00:48:03 +0530

    Code optimized and refractor

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import About from "./components/About";
 import Contact from "./components/Contact";
 import Body from "./components/Body";
-import About from "./components/About";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
