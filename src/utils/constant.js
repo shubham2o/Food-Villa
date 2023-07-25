1e104a91421b0280b2baeaaa80716499252358b5
shubham2o <shubhamsharma.ha@gmail.com>
2023-07-25 11:18:41 +0530

    MENU_API added

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,8 @@
-export const LOGO_CDN_URL = 
-"https://i.playboard.app/p/AAUvwnid3BDOuAtsI4uGV5D5H4LGGfPuPOQD-w88CHLP/default.jpg";
+export const LOGO_CDN_URL =
+    "https://i.playboard.app/p/AAUvwnid3BDOuAtsI4uGV5D5H4LGGfPuPOQD-w88CHLP/default.jpg";
 
-export const IMG_CDN_URL = 
-"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const IMG_CDN_URL =
+    "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const MENU_API =
+    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6406443&lng=77.2746373&restaurantId=";
